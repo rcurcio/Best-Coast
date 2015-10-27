b2068250bfac99a87ceab59c56ee6c6582fdf7d2
toshbar <bartoshsudak@gmail.com>
2015-10-26 21:21:34 -0700

    made comment box better in locations

--- js/locations.js
+++ js/locations.js
@@ -16,3 +16,32 @@ window.onload = function(){
 	change_name();
 	change_description();
 }
+
+//using lists.js to generate user comment list
+//use AJAX to fill in list from database later on
+$(function() {
+ 
+// this sets up a "template" for the list items
+var options = {
+	valueNames: [ 'imgUrl', 'text' ],
+	item: '<li> <div class="commenterImage"><img class="imgUrl"/></div><div class="commentText"> <p class="text"></p> </div></li>'
+};
+                
+//this json is used to generate the comments. each object is a comment -
+//use ajax to get the data from the server and then generated the json and passed it into list.js
+var values = [{
+	imgUrl: 'http://lorempixel.com/50/50/people/5',
+	text: "Wow this food is amazing!"
+},
+	{
+	imgUrl: 'http://lorempixel.com/50/50/people/6',
+	text: "The beaches here are incredible"
+},
+	{
+	imgUrl: 'http://lorempixel.com/50/50/people/7',
+	text: "Don't get the fish tacos at the pier. Just don't."
+}];
+
+	var commentList = new List('comments', options, values); // pass the object in
+});
+        
