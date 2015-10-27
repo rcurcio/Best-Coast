58ca22cadb4e6aa260027a8c4aea9b4413a4b3cc
tamiresbs2108 <tamiresbritodasilva@gmail.com>
2015-10-26 19:55:49 -0700

    eat$drink, stay, events html created

---
+++ js/stay.js
@@ -0,0 +1,13 @@
+var n = 2;
+var url = "#";
+var image = "../images/img1.png";
+
+var myDiv = "<div class='row'><div class='col-lg-12'><h2 class='page-header'>Stay</h2></div>";
+
+for (var i=1; i<=n; i++) {
+		
+	myDiv += "<div class='col-lg-3 col-md-4 col-xs-6 thumb'><a href=" + url + "><img class='img-responsive img-rounded link-home' src=" + image + " alt=''></div>";
+}
+
+myDiv += "</div>";
+document.write(myDiv);
